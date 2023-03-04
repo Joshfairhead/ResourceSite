9abc960773e6d060db585b51d6d8f7d0b97d7379
Maija <hello@maijagrudule.com>
2023-03-04 02:22:10 +0000

    🚀 Init commit + LPL theme + Draft content

---
+++ site/.vitepress/config.js
@@ -0,0 +1,151 @@
+export default {
+    title: "Lunarpunk Labs",
+    description: "",
+    appearance: "dark",
+    head: [
+        ["link", { rel: "icon", type: "favicon", href: "/favicon.ico" }],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "16x16",
+                href: "/favicon-16x16.png",
+            },
+        ],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "32x32",
+                href: "/favicon-32x32.png",
+            },
+        ],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "96x96",
+                href: "/apple-touch-icon.png",
+            },
+        ],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "192x192",
+                href: "/android-chrome-192x192.png",
+            },
+        ],
+        [
+            "link",
+            {
+                rel: "icon",
+                type: "image/png",
+                sizes: "512x512",
+                href: "/android-chrome-512x512.png",
+            },
+        ],
+    ],
+    themeConfig: {
+        siteTitle: "Lunarpunk Labs",
+        logo: "/assets/logo.png",
+        editLink: {
+            pattern:
+                "https://github.com/lunarpunk-labs/resource-site/edit/main/site/:path",
+            text: "Edit this page on GitHub",
+        },
+        socialLinks: [
+            { icon: "github", link: "https://github.com/lunarpunk-labs" },
+            { icon: "twitter", link: "https://twitter.com/lunarpunk_labs" },
+            {
+                icon: {
+                    svg: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25" height="100%" style="display: block; width: auto;"><title>Linktree</title><desc>Linktree Logo Symbol</desc><path d="M13.5108 5.85343L17.5158 1.73642L19.8404 4.11701L15.6393 8.12199H21.5488V11.4268H15.6113L19.8404 15.5345L17.5158 17.8684L11.7744 12.099L6.03299 17.8684L3.70842 15.5438L7.93745 11.4361H2V8.12199H7.90944L3.70842 4.11701L6.03299 1.73642L10.038 5.85343V0H13.5108V5.85343ZM10.038 16.16H13.5108V24.0019H10.038V16.16Z" fill="currentColor"></path></svg>',
+                },
+                link: "https://linktr.ee/lunarpunk_labs",
+            },
+        ],
+        nav: [
+            {
+                text: "Learning Resources",
+                items: [
+                    {
+                        text: "Blockchain First Principles",
+                        link: "/resources/first-principles/",
+                    },
+                    {
+                        text: "How to Guides",
+                        link: "/resources/guides/wallet/wallet-setup",
+                    },
+                ],
+            },
+            {
+                text: "Blog",
+                items: [
+                    { text: "To The Cosmos", link: "https://tothecosmos.org/" },
+                ],
+            },
+        ],
+        sidebar: [
+            {
+                text: "Wallets",
+                collapsable: true,
+                items: [
+                    // { text: "Cosmos Wallet Onboarding", link: "/wallet/cosmos-wallet-onboarding" },
+                    {
+                        text: "Wallet Setup",
+                        link: "/resources/guides/wallet/wallet-setup",
+                    },
+                    {
+                        text: "Wallet Transactions",
+                        link: "/resources/guides/wallet/wallet-transactions",
+                    },
+                    {
+                        text: "Staking Wallet Setup",
+                        link: "/resources/guides/wallet/staking-wallet-setup",
+                    },
+                ],
+            },
+            {
+                text: "Commonwealth",
+                collapsable: true,
+                items: [
+                    {
+                        text: "Sign-up",
+                        link: "/resources/guides/commonwealth/sign-up",
+                    },
+                    {
+                        text: "Discussion",
+                        link: "/resources/guides/commonwealth/discussion",
+                    },
+                    {
+                        text: "Proposals",
+                        link: "/resources/guides/commonwealth/proposals",
+                    },
+                    {
+                        text: "Voting",
+                        link: "/resources/guides/commonwealth/voting",
+                    },
+                    {
+                        text: "Community Spend Proposal",
+                        link: "/resources/guides/commonwealth/community-spend-proposal",
+                    },
+                ],
+            },
+            // {
+            //     text: "Useful Links",
+            //     items: [
+            //         { text: "Example", link: "/" },
+            //     ],
+            // },
+        ],
+        outline: "deep",
+        // footer: {
+        //     message: 'Released under the MIT License.',
+        //     copyright: 'Copyright © 2019-present Evan You'
+        //   }
+    },
+};
---
+++ site/index.md
@@ -0,0 +1,27 @@
+---
+layout: home
+
+hero:
+  name: Lunarpunk Labs
+  # text: Generative Strategy
+  tagline: Hatching generative systems
+  actions:
+    - theme: alt
+      text: Guidance 
+      link: /resources/guides/wallet/wallet-setup
+# features:
+# - icon: 🛠️
+#   title: Simple and minimal, always
+#   details: Lorem ipsum...
+# - icon:
+#     src: /cool-feature-icon.svg
+#   title: Another cool feature
+#   details: Lorem ipsum...
+# - icon:
+#     dark: /dark-feature-icon.svg
+#     light: /light-feature-icon.svg
+#   title: Another cool feature
+#   details: Lorem ipsum...
+---
+
+

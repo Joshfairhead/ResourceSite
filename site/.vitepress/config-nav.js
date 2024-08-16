c541303ff67bce7e4d678abade7170d20ae7bc39
Josh Fairhead <hello@joshafairhead.com>
2024-08-16 17:33:03 +0100

    Config Edits

--- site/.vitepress/config-nav.js
+++ site/.vitepress/config-nav.js
@@ -1,6 +1,19 @@
 export default {
   nav: [
     {
+      text: "Commerce",
+      link: "https://commerce.tothecosmos.org/",
+    },
+    {
+      text: "Correspondence",
+      link: "https://tothecosmos.org/",
+    },
+    {
+      text: "Propsals",
+      link:
+        "https://commonwealth.im/regen/discussion/10002-rfp-tokenomics-20-proposal-adaptive-capital",
+    },
+    {
       text: "Resources",
       items: [
         {
@@ -12,14 +25,6 @@ export default {
           link: "/resources/how/wallet/wallet-setup",
         },
       ],
-    },
-    {
-      text: "Commerce",
-      link: "https://commerce.tothecosmos.org/",
-    },
-    {
-      text: "Correspondence",
-      link: "https://tothecosmos.org/",
     },
   ],
 };
